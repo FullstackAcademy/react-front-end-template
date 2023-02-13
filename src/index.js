6cdf6bacad58748285a88b8fa3ff401c3dfe3e7f
Ithndr <tm21999@gmail.com>
2023-02-13 12:12:21 -0500

    started on links

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import { createRoot } from 'react-dom/client';
 const App = ()=> {
   return (
     <div>
+      <nav className='navBar'>
+        <Link to='/register'>Register</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/activities'>Register</Link>
+        <Link to='/routines'>Register</Link>
+        <Link to='/users/:username/routines'>Register</Link>
+      </nav>
       <h1>Front End</h1>
+      <Routes>
+        <Route path='/register'/>
+        <Route path='/login'/>
+        <Route path='/activities'/>
+        <Route path='/routines'/>
+        <Route path='/users/:username/routines'/>
+      </Routes>
     </div>
   );
 };

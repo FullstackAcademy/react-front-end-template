4bb716ff6a6a6e927e63c67a2fa7fc561df6eec2
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-02-14 15:15:28 -0500

    getting started with app and index.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = ()=> {
+    return (
+      <div>
+        <h1>Header Componenet</h1>
+        <h1>Nav Component</h1>
+        <h1>Routine Component</h1>
+        <h1>login Component</h1>
+      </div>
+    );
+  };
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from '/app.js';
+import App from '/src/App.js';
 
 const root = createRoot(document.querySelector('#root'));
 

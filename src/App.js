3310b0d28bfc5d6f709c7b396e655e805c00f7b9
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-02-15 12:04:11 -0600

    started function to view all routines

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import { getAllRoutines } from './api/fetchRoutine';
 import { Login } from "./components/login"
 import { Register } from './components/register';
+import {ViewRoutine} from './components/routine/ViewRoutine'
+
 
 const App = () => {
   const [user, setUser] = useState({});
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [token, setToken] = useState(null);
+  const [routines, setRoutines] = useState([])
+
+  useEffect(() => {
+    const fetchAllRoutines = async () => {
+      const fetchedRoutines = await getAllRoutines();
+      setRoutines(fetchedRoutines);
+    }
+   fetchAllRoutines()
+  }, [])
+
   return (
     <div>
       <nav className='navBar'>
@@ -15,9 +28,11 @@ const App = () => {
         <Link to='/register'>Register</Link>
       </nav>
       {user.username ? <h3>{`welcome back: ${user.username}`}</h3>:null}
+
       <Routes>
         <Route path='/login' element={<Login user={user} setUser={setUser} token={token} />} />
         <Route path='/register' element={<Register setUser={setUser} setToken={setToken} />} />
+        
       </Routes>
     </div>
   );
